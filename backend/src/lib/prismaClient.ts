12694d4b217217ddbe09ca3c162837875dd13e41
karan singh <karansinghcodes@gmail.com>
2025-08-29 01:00:16 +0530

    setup database with prisma

---
+++ backend/src/lib/prismaClient.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function dbConnection() {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the database successfully.");
+  } catch (error: any) {
+    console.error("Failed to connect to the database:", error.message);
+    throw new Error(error.message);
+  }
+}
+
+export default prisma;
