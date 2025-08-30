419b8b0078bade46d9abdd6da49114f90f39ecc0
karan singh <karansinghcodes@gmail.com>
2025-08-30 22:57:58 +0530

    add signup and signin page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
 
-
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
-
-
   return (
-    <div>
-      Hello
-    </div>
+    <>
 
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
+      </Routes>
+    </>
   );
 };
 
