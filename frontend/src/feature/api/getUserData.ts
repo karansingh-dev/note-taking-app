5e0841447c70c4219501342c7f9909ea3155b4e0
karan singh <karansinghcodes@gmail.com>
2025-09-01 02:38:06 +0530

    error fixed

--- frontend/src/feature/api/getUserData.ts
+++ frontend/src/feature/api/getUserData.ts
@@ -2,11 +2,7 @@ import type { UserDetailsType } from "../../types";
 import { apiCall } from "../../utils/api";
 
 export const getUserData = async () => {
-  const res = await apiCall("/user", "GET", "protected");
-  
+  const res = await apiCall<UserDetailsType, null>("/user", "GET", "protected");
 
-
- return res.data;
-
-
+  return res.data;
 };
